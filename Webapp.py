a097855aef77182db7915e0255ec8dac728dee28
Aleksandar <asavkovic.010@gmail.com>
2024-01-10 21:10:50 +0100

    added second page

--- Webapp.py
+++ Webapp.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
+def home():
     return render_template('home.html')
+
+@app.route('/temperature/')
+def temp():
+    return render_template('Temperature_Graphs.html')
 
 app.run(host='0.0.0.0', port=5000)
