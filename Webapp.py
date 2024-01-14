1957b6ffb7a3464ac5368db3ce17e51459d2aad6
Aleksandar <asavkovic.010@gmail.com>
2024-01-14 21:11:40 +0100

    added mqtt

--- Webapp.py
+++ Webapp.py
@@ -1,4 +1,60 @@
 from flask import Flask, render_template, request
+import paho.mqtt.client as mqtt
+import time
+
+# MQTT broker details
+broker_address = "192.168.178.58"
+broker_port = 1883
+topic = "meritve/#"
+data = {}
+
+
+def on_connect(client, userdata, flags, rc):
+    print(f"Connected with result code {rc}")
+    client.subscribe(topic)
+
+
+
+# Callback when a message is received from the broker
+def on_message(client, userdata, msg):
+    message = msg.payload.decode()
+    #dobijo sam podatak merjenje/3/temp, "32"
+    print(f"Received message on topic '{msg.topic}': {message}")
+    tema = msg.topic
+    podatki = tema.split("/")
+    module_id = podatki[1]
+    vrsta = podatki[2]
+    novi_podatak = int(message)
+
+    module_data = data.get(module_id)
+    if module_data is None:
+        module_data = {}
+
+    stari_podatki = module_data.get(vrsta)
+    if stari_podatki is None:
+        stari_podatki = []
+    stari_podatki.append(novi_podatak)
+
+    module_data[vrsta] = stari_podatki
+    data[module_id] = module_data
+    print(data)
+
+
+def create_mqtt_client():
+    # Create a MQTT client
+    mqtt_client = mqtt.Client()
+
+    # Set callback functions
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message
+
+    # Connect to the broker
+    mqtt_client.connect(broker_address, broker_port, 60)
+
+    # Start the MQTT client in a background thread
+    mqtt_client.loop_start()
+
+
 
 app = Flask(__name__)
 
@@ -24,4 +80,11 @@ def soba():
     room = request.args.get('room', default="???", type=str)
     return render_template('soba.html', soba=room)
 
-app.run(host='0.0.0.0', port=5000)
+
+
+
+if __name__ == "__main__":
+    print('start')
+    create_mqtt_client()
+    app.run(host='0.0.0.0', port=5000)
+    print('end')
