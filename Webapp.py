50798a4968a693e447fe12c7629f5d728cceaed7
Aleksandar <asavkovic.010@gmail.com>
2024-01-14 12:32:42 +0100

    added rooms

--- Webapp.py
+++ Webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -18,5 +18,10 @@ def hum():
 @app.route('/pressure/')
 def press():
     return render_template('Pressure_Graphs.html')
+
+@app.route('/soba')
+def soba():
+    room = request.args.get('room', default="???", type=str)
+    return render_template('soba.html', soba=room)
 
 app.run(host='0.0.0.0', port=5000)
