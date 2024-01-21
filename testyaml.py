8f74b4c0684af41015a2b1ad4652e6c73ef5ced2
Aleksandar <asavkovic.010@gmail.com>
2024-01-21 20:00:35 +0100

    uncommented yaml.py

--- testyaml.py
+++ testyaml.py
@@ -14,55 +14,40 @@ data = {
 	}
 }
 
-bbb = {}
 
-temp = bbb.get('3', {}).get('temp')
-
-if temp == None:
-    pass
-
-else:
-    temp = temp[-1]
-
-print(temp)
+def save_data(data):
+    with open('data.yaml', 'w') as outfile:
+        yaml.dump(data, outfile, default_flow_style=False)    
 
 
+def load_data():
+    with open("data.yaml", 'r') as stream:
+        data_loaded = yaml.safe_load(stream)
+    return data_loaded
 
 
-
-# def save_data(data):
-#     with open('data.yaml', 'w') as outfile:
-#         yaml.dump(data, outfile, default_flow_style=False)    
-
-
-# def load_data():
-#     with open("data.yaml", 'r') as stream:
-#         data_loaded = yaml.safe_load(stream)
-#     return data_loaded
-
-
-# def main():
-#     # save_data(data)
-#     data = load_data()
-#     print(data["24"])
-#     print(data.get("24", {}).get("hum"))
-#     print(json.dumps(data, indent=2))
+def main():
+    # save_data(data)
+    data = load_data()
+    print(data["24"])
+    print(data.get("24", {}).get("hum"))
+    print(json.dumps(data, indent=2))
     
     
     
-#     # print('start')
-#     # print('writing the file')
-#     # write()
-#     # print('file writen')
-#     # print('reading file')
-#     # read()
-#     # print('returning the read values: ')
-#     # print(data)
-#     # print('end')
+    # print('start')
+    # print('writing the file')
+    # write()
+    # print('file writen')
+    # print('reading file')
+    # read()
+    # print('returning the read values: ')
+    # print(data)
+    # print('end')
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
 
-# # print(data == data_loaded)
-# # print(data_loaded)
-# # print(data_loaded["B"])
+# print(data == data_loaded)
+# print(data_loaded)
+# print(data_loaded["B"])
